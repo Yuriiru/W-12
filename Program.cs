b3e75cbb831c6bd5819eadab8655b7c0e2bc932a
Yuriiru <98612792+Yuriiru@users.noreply.github.com>
2023-05-06 10:49:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+    class EventDelegateClass
+    {
+        private string name; // создание переменной
+
+        public delegate void EventDelegate(string txt); // создание делегата +
+
+        public EventDelegateClass(string name) // конструктор класса
+        {
+            this.name = name;
+        }
+
+        public event EventDelegate Event; // определение события
+        public void RaiseMyEvent() // с помощью метода + (вызов события)
+        {
+            if (Event != null)
+            {
+                Event(name);
+            }
+        }
+    }
+
+class KakoyToClass
+{
+        private string text; //создание переменной
+        public KakoyToClass(string metodtext) // конструктор класса
+        {
+            text = metodtext;
+        }
+        public void show(string objname) // метод +
+        {
+            Console.WriteLine("Объект, сгенерировавший событие: " + objname);
+            Console.WriteLine("Сообщение: " + text);
+        }
+}
+
+class Program
+{
+        static void Main()
+        {
+            EventDelegateClass kakoyToOdject1 = new EventDelegateClass("KakoyToOdject1"); // создаем объекты первого класса
+            EventDelegateClass kakoyToOdject2 = new EventDelegateClass("KakoyToOdject2");
+
+            KakoyToClass KakoyToClassObject = new KakoyToClass("KakoyToClassObject"); // создание объекта второго класса
+
+            kakoyToOdject1.Event += KakoyToClassObject.show;
+            kakoyToOdject2.Event += KakoyToClassObject.show;
+
+            kakoyToOdject1.RaiseMyEvent();
+            Console.WriteLine();
+
+            kakoyToOdject2.RaiseMyEvent();
+            Console.WriteLine();
+        }
+}
+
+// где обработчик события
+
+
